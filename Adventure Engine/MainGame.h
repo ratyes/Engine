c34463e77e319d4bf20fb70d887a498a62aebec5
ratyes <dylan_deka@yahoo.com>
2017-07-23 23:20:14 +0200

    Add files via upload

---
+++ Adventure Engine/MainGame.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <SDL/SDL.h>
+#include <GL/glew.h>
+
+enum class GameState {PLAY,EXIT};
+
+class MainGame
+{
+public:
+	MainGame();
+	~MainGame();
+
+
+	void run();
+
+
+
+private:
+	void initSystems();
+	void processInput();
+
+	void gameLoop();
+
+	SDL_Window* m_window;
+	char* m_screenTitle;
+	int m_screenWidth;
+	int m_screenHeight;
+	GameState m_gameState;
+
+};
+
